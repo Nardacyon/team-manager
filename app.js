46f6b5ebaa921e6ab2c6841d08506345244b21b6
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-09-11 22:10:18 -0700

    created inquirer prompt and render html

--- app.js
+++ app.js
@@ -1,18 +1,79 @@
+//extended employee classes
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+//npm dependencies *MAKE SURE TO INSTALL THEM*
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//render gathered information 
+const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
+const { prompt } = require("inquirer");
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
-
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+let employeeData;
+let employeeList = [];
+
+function gatherEmployeeData() {
+    inquirer.prompt(questions).then(function (answers) {
+        createEmployeeObject(answers);
+        inquirer.prompt([
+            {
+                type: "confirm",
+                name: "confirmation",
+                message: "Would you like to continue editing?\n",
+                default: true
+            }
+        ]).then(function (res) {
+            if (res.confirmation) {
+                gatherEmployeeData();
+            } else {
+                createMarkdown();
+            }
+        });
+    });
+}
+
+function createEmployeeObject(answers) {
+    let role = answers.role;
+    switch (role) {
+        case "Engineer":
+            let newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            employeeList.push(newEngineer);
+            break;
+        case "Intern":
+            let newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            employeeList.push(newIntern);
+            break;
+        case "Manager":
+            let newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            employeeList.push(newManager);
+            break;
+    }
+}
+
+async function createMarkdown() {
+    try {
+        const getHTML = await render(employeeList);
+
+        createHTML(getHTML);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function createHTML(getHTML) {
+    fs.writeFile(outputPath, getHTML, function (err) {
+        if (err) console.log(err);
+    });
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +94,76 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const questions = ([
+    {
+        type: "list",
+        name: "Selected",
+        message: "What would you like to do?\n",
+        choices: [
+            "Add an Employee",
+            // "Remove an Employee",
+            "Exit"
+        ]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?\n",
+        when: (answers) => answers.Selected === "Add an Employee"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's numeric ID?\n",
+        when: (answers) => answers.Selected === "Add an Employee"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?\n",
+        when: (answers) => answers.Selected === "Add an Employee"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?\n",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        when: (answers) => answers.Selected === "Add an Employee"
+    },
+    //Engineer specific
+    {
+        type: "input",
+        name: "github",
+        message: "What is their GitHub username?\n",
+        when: (answers) => answers.role === "Engineer"
+    },
+    //Intern specific
+    {
+        type: "input",
+        name: "school",
+        message: "What school is the intern currently attending?\n",
+        when: (answers) => answers.role === "Intern"
+    },
+    //Manager specific
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?\n",
+        when: (answers) => answers.role === "Manager"
+        // validate 
+    },
+    {
+        type: "input",
+        name: "team",
+        message: "What team does this employee operate in?\n",
+        when: (answers) => answers.Selected === "Add an Employee"
+    },
+]);
+
+// gatherData();
+gatherEmployeeData();
